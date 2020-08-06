6b5c853c8714d7b4ff20a1d84536ee77097be1c5
Hal Seki <hal@Hals-MacBook-Pro.local>
2020-08-06 23:13:10 +0900

    fix #21 add auto break

--- scripts/gs_template.js
+++ scripts/gs_template.js
@@ -1,24 +1,23 @@
 // メッセージテンプレート
 // GSTemplate = loadGSTemplate();
 
-loadGSTemplate = function() {
-  var GSTemplate = function(spreadsheet) {
+loadGSTemplate = function () {
+  var GSTemplate = function (spreadsheet) {
     this.spreadsheet = spreadsheet;
 
     // メッセージテンプレート設定 
     this.sheet = this.spreadsheet.getSheetByName('_メッセージ');
-    if(!this.sheet) {
+    if (!this.sheet) {
       this.sheet = this.spreadsheet.insertSheet('_メッセージ');
-      if(!this.sheet) {
+      if (!this.sheet) {
         throw "エラー: メッセージシートを作れませんでした";
-      }
-      else {
+      } else {
         var now = DateUtils.now();
         this.sheet.getRange("A1:N2").setValues([
           [
             "出勤", "出勤更新", "退勤", "退勤更新", "休憩", "休暇", "休暇取消",
             "出勤中", "出勤なし", "休暇中", "休暇なし", "出勤確認", "退勤確認",
-            "休憩エラー"
+            "休憩エラー", "退勤と休憩", "退勤更新と休憩"
           ],
           [
             "<@#1> Good morning (#2)!", "<@#1> I changed starting time to #2",
@@ -28,7 +27,9 @@ loadGSTemplate = function() {
             "#1 is working", "All staffs are working",
             "#2 is having a holiday at #1", "No one is having a holiday at #1",
             "Is today holiday? #1", "Did you finish working today? #1",
-            "[Error] You have not started working today!"
+            "[Error] You have not started working today!",
+            "<@#1> Great work! I added 60 mins break for you (#2)",
+            "<@#1> I changed leaving time to #2 and added 6 mins break for you"
           ]
         ]);
       }
@@ -36,31 +37,31 @@ loadGSTemplate = function() {
   };
 
   // テンプレートからメッセージを生成
-  GSTemplate.prototype.template = function(label) {
+  GSTemplate.prototype.template = function (label) {
     var labels = this.sheet.getRange("A1:Z1").getValues()[0];
-    for(var i = 0; i < labels.length; ++i) {
-      if(labels[i] == label) {
+    for (var i = 0; i < labels.length; ++i) {
+      if (labels[i] == label) {
         var template = _.sample(
           _.filter(
-            _.map(this.sheet.getRange(String.fromCharCode(i+65)+'2:'+(String.fromCharCode(i+65))).getValues(), function(v) {
-            return v[0];
-          }),
-            function(v) {
+            _.map(this.sheet.getRange(String.fromCharCode(i + 65) + '2:' + (String.fromCharCode(i + 65))).getValues(), function (v) {
+              return v[0];
+            }),
+            function (v) {
               return !!v;
             }
-        )
+          )
         );
 
         var message = template;
         for (var i = 1; i < arguments.length; i++) {
           var arg = arguments[i]
-          if(_.isArray(arg)) {
-            arg = _.map(arg, function(u) {
-              return "<@"+u+">";
+          if (_.isArray(arg)) {
+            arg = _.map(arg, function (u) {
+              return "<@" + u + ">";
             }).join(', ');
           }
 
-          message = message.replace("#"+i, arg);
+          message = message.replace("#" + i, arg);
         }
 
         return message;
@@ -72,6 +73,6 @@ loadGSTemplate = function() {
   return GSTemplate;
 };
 
-if(typeof exports !== 'undefined') {
+if (typeof exports !== 'undefined') {
   exports.GSTemplate = loadGSTemplate();
 }
--- scripts/timesheets.js
+++ scripts/timesheets.js
@@ -2,25 +2,25 @@
 // Timesheets = loadTimesheets();
 
 loadTimesheets = function (exports) {
-  var Timesheets = function(storage, settings, responder) {
+  var Timesheets = function (storage, settings, responder) {
     this.storage = storage;
     this.responder = responder;
     this.settings = settings;
 
     var self = this;
-    this.responder.on('receiveMessage', function(username, message) {
+    this.responder.on('receiveMessage', function (username, message) {
       self.receiveMessage(username, message);
     });
   };
 
   // メッセージを受信する
-  Timesheets.prototype.receiveMessage = function(username, message) {
+  Timesheets.prototype.receiveMessage = function (username, message) {
     // 日付は先に処理しておく
     this.date = DateUtils.parseDate(message);
     this.time = DateUtils.parseTime(message);
     this.minutes = DateUtils.parseMinutes(message)
     this.datetime = DateUtils.normalizeDateTime(this.date, this.time);
-    if(this.datetime !== null) {
+    if (this.datetime !== null) {
       this.dateStr = DateUtils.format("Y/m/d", this.datetime);
       this.datetimeStr = DateUtils.format("Y/m/d H:M", this.datetime);
     }
@@ -39,28 +39,31 @@ loadTimesheets = function (exports) {
     ];
 
     // メッセージを元にメソッドを探す
-    var command = _.find(commands, function(ary) {
-      return(ary && message.match(ary[1]));
+    var command = _.find(commands, function (ary) {
+      return (ary && message.match(ary[1]));
     });
 
     // メッセージを実行
-    if(command && this[command[0]]) {
+    if (command && this[command[0]]) {
       return this[command[0]](username, message);
     }
   }
 
   // 出勤
-  Timesheets.prototype.actionSignIn = function(username, message) {
-    if(this.datetime) {
+  Timesheets.prototype.actionSignIn = function (username, message) {
+    if (this.datetime) {
       var data = this.storage.get(username, this.datetime);
-      if(!data.signIn || data.signIn === '-') {
-        this.storage.set(username, this.datetime, {signIn: this.datetime});
+      if (!data.signIn || data.signIn === '-') {
+        this.storage.set(username, this.datetime, {
+          signIn: this.datetime
+        });
         this.responder.template("出勤", username, this.datetimeStr);
-      }
-      else {
+      } else {
         // 更新の場合は時間を明示する必要がある
-        if(!!this.time) {
-          this.storage.set(username, this.datetime, {signIn: this.datetime});
+        if (!!this.time) {
+          this.storage.set(username, this.datetime, {
+            signIn: this.datetime
+          });
           this.responder.template("出勤更新", username, this.datetimeStr);
         }
       }
@@ -68,139 +71,171 @@ loadTimesheets = function (exports) {
   };
 
   // 退勤
-  Timesheets.prototype.actionSignOut = function(username, message) {
-    if(this.datetime) {
+  Timesheets.prototype.actionSignOut = function (username, message) {
+    if (this.datetime) {
       var data = this.storage.get(username, this.datetime);
-      if(!data.signOut || data.signOut === '-') {
-        this.storage.set(username, this.datetime, {signOut: this.datetime});
-        this.responder.template("退勤", username, this.datetimeStr);
-      }
-      else {
+      if (!data.signIn || data.signIn === '-') {
+        // まだ出勤前である
+        this.responder.template("休憩エラー", username, "");
+      } else if (!data.signOut || data.signOut === '-') {
+        this.storage.set(username, this.datetime, {
+          signOut: this.datetime
+        });
+        // 5時間以上働いていて、休憩が入っていなければ休憩を更新する
+        if (!data.break && (this.datetime - data.signIn) > (5 * 60 * 60 * 1000)) {
+          // break 入力
+          this.storage.set(username, this.datetime, {
+            break: 60
+          });
+          this.responder.template("退勤と休憩", username, this.datetimeStr);
+        } else {
+          this.responder.template("退勤", username, this.datetimeStr);
+        }
+      } else {
         // 更新の場合は時間を明示する必要がある
-        if(!!this.time) {
-          this.storage.set(username, this.datetime, {signOut: this.datetime});
-          this.responder.template("退勤更新", username, this.datetimeStr);
+        if (!!this.time) {
+          this.storage.set(username, this.datetime, {
+            signOut: this.datetime
+          });
+          // 5時間以上働いていて、休憩が入っていなければ休憩を更新する
+          if (!data.break && (this.datetime - data.signIn) > (5 * 60 * 60 * 1000)) {
+            // break 入力
+            this.storage.set(username, this.datetime, {
+              break: 60
+            });
+            this.responder.template("退勤更新と休憩", username, this.datetimeStr);
+          } else {
+            this.responder.template("退勤更新", username, this.datetimeStr);
+          }
         }
       }
     }
   };
 
   // 休憩
-  Timesheets.prototype.actionBreak = function(username, time) {
+  Timesheets.prototype.actionBreak = function (username, time) {
     if (this.minutes) {
       var data = this.storage.get(username, this.datetime);
-      if(!data.signIn || data.signIn === '-') {
+      if (!data.signIn || data.signIn === '-') {
         // まだ出勤前である
-        this.responder.template("休憩エラー", username, "" );
+        this.responder.template("休憩エラー", username, "");
       } else {
         // break 入力
-        this.storage.set(username, this.datetime, {break: this.minutes});
+        this.storage.set(username, this.datetime, {
+          break: this.minutes
+        });
         this.responder.template("休憩", username, this.minutes + "分");
       }
     }
   };
 
   // 休暇申請
-  Timesheets.prototype.actionOff = function(username, message) {
-    if(this.date) {
-      var dateObj = new Date(this.date[0], this.date[1]-1, this.date[2]);
+  Timesheets.prototype.actionOff = function (username, message) {
+    if (this.date) {
+      var dateObj = new Date(this.date[0], this.date[1] - 1, this.date[2]);
       var data = this.storage.get(username, dateObj);
-      if(!data.signOut || data.signOut === '-') {
-        this.storage.set(username, dateObj, {signIn: '-', signOut: '-', note: message});
+      if (!data.signOut || data.signOut === '-') {
+        this.storage.set(username, dateObj, {
+          signIn: '-',
+          signOut: '-',
+          note: message
+        });
         this.responder.template("休暇", username, DateUtils.format("Y/m/d", dateObj));
       }
     }
   };
 
   // 休暇取消
-  Timesheets.prototype.actionCancelOff = function(username, message) {
-    if(this.date) {
-      var dateObj = new Date(this.date[0], this.date[1]-1, this.date[2]);
+  Timesheets.prototype.actionCancelOff = function (username, message) {
+    if (this.date) {
+      var dateObj = new Date(this.date[0], this.date[1] - 1, this.date[2]);
       var data = this.storage.get(username, dateObj);
-      if(!data.signOut || data.signOut === '-') {
-        this.storage.set(username, dateObj, {signIn: null, signOut: null, note: message});
+      if (!data.signOut || data.signOut === '-') {
+        this.storage.set(username, dateObj, {
+          signIn: null,
+          signOut: null,
+          note: message
+        });
         this.responder.template("休暇取消", username, DateUtils.format("Y/m/d", dateObj));
       }
     }
   };
 
   // 出勤中
-  Timesheets.prototype.actionWhoIsIn = function(username, message) {
+  Timesheets.prototype.actionWhoIsIn = function (username, message) {
     var dateObj = DateUtils.toDate(DateUtils.now());
-    var result = _.compact(_.map(this.storage.getByDate(dateObj), function(row) {
+    var result = _.compact(_.map(this.storage.getByDate(dateObj), function (row) {
       return _.isDate(row.signIn) && !_.isDate(row.signOut) ? row.user : undefined;
     }));
 
-    if(_.isEmpty(result)) {
+    if (_.isEmpty(result)) {
       this.responder.template("出勤なし");
-    }
-    else {
+    } else {
       this.responder.template("出勤中", result.sort().join(', '));
     }
   };
 
   // 休暇中
-  Timesheets.prototype.actionWhoIsOff = function(username, message) {
+  Timesheets.prototype.actionWhoIsOff = function (username, message) {
     var dateObj = DateUtils.toDate(DateUtils.now());
     var dateStr = DateUtils.format("Y/m/d", dateObj);
-    var result = _.compact(_.map(this.storage.getByDate(dateObj), function(row){
+    var result = _.compact(_.map(this.storage.getByDate(dateObj), function (row) {
       return row.signIn === '-' ? row.user : undefined;
     }));
 
     // 定休の処理
     var wday = dateObj.getDay();
     var self = this;
-    _.each(this.storage.getUsers(), function(username) {
-      if(_.contains(self.storage.getDayOff(username), wday)) {
+    _.each(this.storage.getUsers(), function (username) {
+      if (_.contains(self.storage.getDayOff(username), wday)) {
         result.push(username);
       }
     });
     result = _.uniq(result);
 
-    if(_.isEmpty(result)) {
+    if (_.isEmpty(result)) {
       this.responder.template("休暇なし", dateStr);
-    }
-    else {
+    } else {
       this.responder.template("休暇中", dateStr, result.sort().join(', '));
     }
   };
 
   // 出勤していない人にメッセージを送る
-  Timesheets.prototype.confirmSignIn = function(username, message) {
+  Timesheets.prototype.confirmSignIn = function (username, message) {
     var self = this;
-    var holidays = _.compact(_.map((this.settings.get("休日") || "").split(','), function(s) {
+    var holidays = _.compact(_.map((this.settings.get("休日") || "").split(','), function (s) {
       var date = DateUtils.parseDateTime(s);
       return date ? DateUtils.format("Y/m/d", date) : undefined;
     }));
     var today = DateUtils.toDate(DateUtils.now());
 
     // 休日ならチェックしない
-    if(_.contains(holidays, DateUtils.format("Y/m/d",today))) return;
+    if (_.contains(holidays, DateUtils.format("Y/m/d", today))) return;
 
     var wday = DateUtils.now().getDay();
-    var signedInUsers = _.compact(_.map(this.storage.getByDate(today), function(row) {
+    var signedInUsers = _.compact(_.map(this.storage.getByDate(today), function (row) {
       var signedIn = _.isDate(row.signIn);
       var off = (row.signIn === '-') || _.contains(self.storage.getDayOff(row.user), wday);
       return (signedIn || off) ? row.user : undefined;
     }));
     var users = _.difference(this.storage.getUsers(), signedInUsers);
 
-    if(!_.isEmpty(users)) {
+    if (!_.isEmpty(users)) {
       this.responder.template("出勤確認", users.sort());
     }
 
     // バージョンチェックを行う
-    if(typeof checkUpdate == 'function') checkUpdate(this.responder);
+    if (typeof checkUpdate == 'function') checkUpdate(this.responder);
   };
 
   // 退勤していない人にメッセージを送る
-  Timesheets.prototype.confirmSignOut = function(username, message) {
+  Timesheets.prototype.confirmSignOut = function (username, message) {
     var dateObj = DateUtils.toDate(DateUtils.now());
-    var users = _.compact(_.map(this.storage.getByDate(dateObj), function(row) {
+    var users = _.compact(_.map(this.storage.getByDate(dateObj), function (row) {
       return _.isDate(row.signIn) && !_.isDate(row.signOut) ? row.user : undefined;
     }));
 
-    if(!_.isEmpty(users)) {
+    if (!_.isEmpty(users)) {
       this.responder.template("退勤確認", users.sort());
     }
   };
@@ -208,6 +243,6 @@ loadTimesheets = function (exports) {
   return Timesheets;
 };
 
-if(typeof exports !== 'undefined') {
+if (typeof exports !== 'undefined') {
   exports.Timesheets = loadTimesheets();
 }
