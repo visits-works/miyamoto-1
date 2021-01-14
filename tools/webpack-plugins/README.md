fc98a37271a4b7cf5bccd2c2c89bdfd80ef733d9
MaySoMusician <maysomusician@gmail.com>
2021-01-14 23:08:02 +0900

    Add webpack plugin source code that is required changing

---
+++ tools/webpack-plugins/README.md
@@ -0,0 +1,10 @@
+2021年1月時点のGASの仕様に対応するように、以下のWebpackプラグインを改造
+
+- `gas-entry-generator.js`
+  - 本家: [fossamagna/gas-entry-generator](https://github.com/fossamagna/gas-entry-generator)
+  - 元コミット: [`ba3dd8bd6d0a7b2b94050bd91d2ac5de05979234`](https://github.com/fossamagna/gas-entry-generator/commit/ba3dd8bd6d0a7b2b94050bd91d2ac5de05979234)
+  - ライセンス: MIT License
+- `gas-webpack-plugin.hs`
+  - 本家: [fossamagna/gas-webpack-plugin](https://github.com/fossamagna/gas-webpack-plugin)
+  - 元コミット: [`56a5c897c7ca12c9a4c4aa03de45750e310bee4a`](https://github.com/fossamagna/gas-webpack-plugin/commit/56a5c897c7ca12c9a4c4aa03de45750e310bee4a)
+  - ライセンス: MIT License
---
+++ tools/webpack-plugins/gas-webpack-plugin.js
@@ -0,0 +1,116 @@
+'use strict';
+
+// Original author: fossamagna <fossamagna2@gmail.com>
+//  - https://github.com/fossamagna/gas-webpack-plugin
+//  - Licensed under MIT License
+//  - Parent commit: 56a5c897c7ca12c9a4c4aa03de45750e310bee4a
+
+const { generate } = require('./gas-entry-generator.js');
+const { SourceMapSource, RawSource } = require('webpack-sources');
+const Dependency = require('webpack/lib/Dependency');
+const minimatch = require('minimatch');
+const path = require('path');
+
+const defaultOptions = {
+  comment: true,
+  autoGlobalExportsFiles: [],
+};
+
+function GasPlugin(options) {
+  this.options = Object.assign({}, defaultOptions, options || {});
+}
+
+function gasify(compilation, chunk, entryFunctions) {
+  chunk.files.forEach(function (filename) {
+    const asset = compilation.assets[filename];
+    let source, map;
+    if (asset.sourceAndMap) {
+      let sourceAndMap = asset.sourceAndMap();
+      source = sourceAndMap.source;
+      map = sourceAndMap.map;
+    } else {
+      source = asset.source();
+      map = typeof asset.map === 'function' ? asset.map() : null;
+    }
+
+    const entries = chunk
+      .getModules()
+      .filter((module) => !!entryFunctions.get(module.id))
+      .map((module) => entryFunctions.get(module.id).entryPointFunctions)
+      .filter((entries) => !!entries)
+      .join('\n');
+
+    const gasify = entries + source;
+    compilation.assets[filename] = map
+      ? new SourceMapSource(gasify, filename, map)
+      : new RawSource(gasify);
+  });
+}
+
+class GasDependency extends Dependency {
+  constructor(module) {
+    super();
+    this.module = module;
+  }
+}
+
+GasDependency.Template = class GasDependencyTemplate {
+  constructor(options) {
+    this.comment = options.comment;
+    this.patterns = options.patterns;
+    this.entryFunctions = new Map();
+  }
+
+  apply(dep, source) {
+    const module = dep.module;
+    const options = {
+      comment: this.comment,
+      autoGlobalExports:
+        module.resource &&
+        this.patterns.some((file) => minimatch(module.resource, file)),
+    };
+    const output = generate(source.source(), options);
+    this.entryFunctions.set(module.id, output);
+    if (output.globalAssignments) {
+      source.insert(source.size(), output.globalAssignments);
+    }
+  }
+};
+
+GasPlugin.prototype.apply = function (compiler) {
+  const context = compiler.options.context;
+  const patterns = this.options.autoGlobalExportsFiles.map((file) =>
+    path.isAbsolute(file) ? file : path.resolve(context, file)
+  );
+  const plugin = { name: 'GasPlugin' };
+  const compilationHook = (compilation) => {
+    const gasDependencyTemplate = new GasDependency.Template({
+      comment: this.options.comment,
+      patterns,
+    });
+
+    compilation.dependencyTemplates.set(GasDependency, gasDependencyTemplate);
+
+    compilation.hooks.buildModule.tap('GasPlugin', (module) => {
+      module.addDependency(new GasDependency(module));
+    });
+
+    compilation.hooks.optimizeChunkAssets.tapAsync(
+      plugin,
+      (chunks, callback) => {
+        chunks.forEach((chunk) => {
+          gasify(compilation, chunk, gasDependencyTemplate.entryFunctions);
+        });
+        callback();
+      }
+    );
+  };
+
+  if (compiler.hooks) {
+    compiler.hooks.compilation.tap(plugin, compilationHook);
+  } else {
+    compiler.plugin('compilation', compilationHook);
+  }
+};
+
+module.exports = GasPlugin;
